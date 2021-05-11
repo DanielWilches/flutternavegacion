e9f970a2c8dc42182767ac7cd07aea34a15fdc64
daniel <danielprietowilches@gmail.com>
2021-05-11 08:16:36 -0500

    prueba_pages

--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,6 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:rickandmortin/login/Login.dart';
+import 'package:rickandmortin/pages/Login.dart';
 
 void main() {
   runApp(MaterialApp(
